84ba65ba5da1e1d10daf804fcb66da09e93ee279
djdw04 <144089704+djdw04@users.noreply.github.com>
2024-02-02 09:18:51 +0800

    Final

---
+++ src/ZombiePlant.java
@@ -0,0 +1,26 @@
+public class ZombiePlant {
+    private int maxPotency;
+    private int treatmentsNeeded;
+
+    public ZombiePlant(int maxPotency, int treatmentsNeeded) {
+        this.maxPotency = maxPotency;
+        this.treatmentsNeeded = treatmentsNeeded;
+    }
+
+    public int treatmentsNeeded() {
+        return treatmentsNeeded;
+    }
+
+    public boolean isDangerous() {
+        return treatmentsNeeded > 0;
+    }
+
+    public void treat(int potency) {
+        if (potency <= maxPotency && treatmentsNeeded > 0 && potency > 1) {
+            treatmentsNeeded--;
+        }
+        if (potency > maxPotency){
+            treatmentsNeeded++;
+        }
+    }
+}
